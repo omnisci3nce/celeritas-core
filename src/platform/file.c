a1209776eb50b1488f02912a6c74481f107c0284
Omniscient <17525998+omnisci3nce@users.noreply.github.com>
2024-02-24 23:12:51 +1100

    readd error text to file loading

--- src/platform/file.c
+++ src/platform/file.c
@@ -1,18 +1,21 @@
 #include "file.h"
 
+#include <errno.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "log.h"
+
 const char *string_from_file(const char *path) {
   FILE *f = fopen(path, "rb");
   if (f == NULL) {
-    // ERROR("Error reading file: %s. errno: %d", path, errno);
+    ERROR("Error reading file: %s. errno: %d", path, errno);
     return NULL;
   }
   if (ferror(f)) {
-    // ERROR("Error reading file: %s. errno: %d", path, errno);
+    ERROR("Error reading file: %s. errno: %d", path, errno);
     return NULL;
   }
   fseek(f, 0, SEEK_END);
