d919e8cb785c165ee700907b74672eb586c55d29
Omniscient <17525998+omnisci3nce@users.noreply.github.com>
2024-02-25 13:31:17 +1100

    add an arena reset to free everything

--- src/std/mem.c
+++ src/std/mem.c
@@ -9,8 +9,8 @@
 #endif
 
 void* arena_alloc_align(arena* a, size_t size, size_t align) {
-  ptrdiff_t padding = -(uintptr_t)a->begin & (align - 1);
-  ptrdiff_t available = a->end - a->begin - padding;
+  ptrdiff_t padding = -(uintptr_t)a->curr & (align - 1);
+  ptrdiff_t available = a->end - a->curr - padding;
   TRACE("Padding %td available %td", padding, available);
   if (available < 0 || (ptrdiff_t)size > available) {
     ERROR_EXIT("Arena ran out of memory\n");
@@ -22,5 +22,9 @@ void* arena_alloc_align(arena* a, size_t size, size_t align) {
 void* arena_alloc(arena* a, size_t size) { return arena_alloc_align(a, size, DEFAULT_ALIGNMENT); }
 
 arena arena_create(void* backing_buffer, size_t capacity) {
-  return (arena){ .begin = backing_buffer, .end = backing_buffer + (ptrdiff_t)capacity };
+  return (arena){ .begin = backing_buffer, .curr = backing_buffer, .end = backing_buffer + (ptrdiff_t)capacity };
+}
+
+void arena_free_all(arena *a) {
+  a->curr = a->begin; // pop everything at once and reset to the start.
 }
--- src/std/mem.h
+++ src/std/mem.h
@@ -14,9 +14,11 @@
 // Inspired by https://nullprogram.com/blog/2023/09/27/
 typedef struct arena {
   char* begin;
+  char* curr;
   char* end;
 } arena;
 
 arena arena_create(void* backing_buffer, size_t capacity);
-void *arena_alloc(arena* a, size_t size);
-void *arena_alloc_align(arena* a, size_t size, size_t align);
+void* arena_alloc(arena* a, size_t size);
+void* arena_alloc_align(arena* a, size_t size, size_t align);
+void arena_free_all(arena* a);
