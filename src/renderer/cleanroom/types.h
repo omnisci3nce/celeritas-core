b4cb698d177ada97c976eab059eabedd433abfd0
omniscient <17525998+omnisci3nce@users.noreply.github.com>
2024-03-28 21:54:22 +1100

    sphere args

--- src/renderer/cleanroom/types.h
+++ src/renderer/cleanroom/types.h
@@ -130,7 +130,7 @@ void gpu_buffer_upload();
 //      - dont need to worry about uploading mesh data
 //      - very useful for debugging
 void imm_draw_cuboid();
-void imm_draw_sphere();
+void imm_draw_sphere(vec3 pos, f32 radius, vec4 colour);
 void imm_draw_camera_frustum();
 static void imm_draw_model(const char* model_filepath); // tracks internally whether the model is loaded
 
