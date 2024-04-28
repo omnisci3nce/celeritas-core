8407723dfc12d302f6b696f702a5ae9caaf219c1
omnisci3nce <omniscient.oce@gmail.com>
2024-04-28 21:55:08 +1000

    delete some comments

--- src/renderer/ral_types.h
+++ src/renderer/ral_types.h
@@ -115,36 +115,3 @@ typedef enum gpu_cull_mode {
 // 3 - you don't need to know how the renderer works at all
 // 2 - you need to know how the overall renderer is designed
 // 1 - you need to understand graphics API specifics
-
-/* ral.h */
-
-// command buffer gubbins
-
-/* --- Backends */
-
-// struct vulkan_backend {
-//   gpu_pipeline static_opaque_pipeline;
-//   gpu_pipeline skinned_opaque_pipeline;
-// };
-
-/* --- Renderer layer */
-/* render.h */
-
-
-// Drawing
-
-// void draw_mesh(gpu_cmd_encoder* encoder, mesh* mesh) {
-//   encode_set_vertex_buffer(encoder, mesh->vertex_buffer);
-//   encode_set_index_buffer(encoder, mesh->index_buffer);
-//   encode_draw_indexed(encoder, mesh->index_count)
-//   // vkCmdDrawIndexed
-// }
-
-// void draw_scene(arena* frame, model_darray* models, renderer* ren, camera* camera,
-//                 transform_hierarchy* tfh, scene* scene) {
-//                   // set the pipeline first
-//                   encode_set_pipeline()
-//                   // in open this sets the shader
-//                   // in vulkan it sets the whole pipeline
-
-// }
