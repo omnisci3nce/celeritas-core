774fc54355abe70a1ba045ade99649ba0e98c930
omniscient <17525998+omnisci3nce@users.noreply.github.com>
2024-07-21 16:17:26 +1000

    start adding rust bindgen

---
+++ bindgen/rust/build.rs
@@ -0,0 +1,53 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    // Tell cargo to look for shared libraries in the specified directory
+    println!("cargo:rustc-link-search=../../build/macosx/arm64/debug");
+
+    // Tell cargo to tell rustc to link the system bzip2
+    // shared library.
+    println!("cargo:rustc-link-lib=core_static");
+    println!("cargo:rustc-link-lib=glfw");
+
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header("../../include/amalgamation.h")
+        // -- our code
+        .clang_arg("-I../../src")
+        .clang_arg("-I../../src/core")
+        .clang_arg("-I../../src/maths")
+        .clang_arg("-I../../src/new_render")
+        .clang_arg("-I../../src/platform")
+        .clang_arg("-I../../src/ral")
+        .clang_arg("-I../../src/ral/backends/opengl")
+        .clang_arg("-I../../src/resources")
+        .clang_arg("-I../../src/std")
+        .clang_arg("-I../../src/std/containers")
+        .clang_arg("-I../../src/systems")
+        // -- dependencies
+        .clang_arg("-I../../deps/cgltf")
+        .clang_arg("-I../../deps/glfw-3.3.8/include/GLFW")
+        .clang_arg("-I../../deps/glad/include")
+        .clang_arg("-I../../deps/stb_image")
+        .clang_arg("-I../../deps/stb_image_write")
+        .clang_arg("-I../../deps/stb_truetype")
+        .generate_inline_functions(true)
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
--- scripts/amalgamation/gen_amalgamation.py
+++ scripts/amalgamation/gen_amalgamation.py
@@ -2,23 +2,58 @@
 #
 # This makes including and linking Celeritas very easy.
 import re
-import sys
+import os
+from pathlib import Path
 
-def find_pub_functions(filepath):
+categories = {
+    "RAL": "src/ral",
+    "Render": "src/new_render",
+    "Maths": "src/maths"
+}
+
+def find_pub_functions_in_folder(folder_path):
+    functions = []
+    for filename in os.listdir(folder_path):
+        filepath = os.path.join(folder_path, filename)
+        if os.path.isfile(filepath):
+            file_funcs = find_pub_functions_in_file(filepath)
+            functions.extend(file_funcs)
+
+    return functions
+
+def find_pub_functions_in_file(file_path):
     pattern = r'PUB\s+(\w+\s+)*(\w+)\s+(\w+)\s*\((.*?)\)'
 
-    with open(filepath, 'r') as file:
+    with open(file_path, 'r') as file:
         content = file.read()
 
     matches = re.finditer(pattern, content, re.MULTILINE)
 
+    # Collect all the functions into an array
+    functions = []
     for match in matches:
-        print(match.group(0))
+        signature = match.group(0)
+        if signature.startswith("PUB "):
+            signature = signature[4:]
+
+        print(signature)
+        functions.append(signature)
+
+    return functions
+
+def generate_header():
+    header_path = "celeritas.h"
+
+    script_dir = Path(__file__).resolve().parent
+    grandparent_dir = script_dir.parents[1]
+
+    with open(header_path, 'w') as export_file:
+        for category in categories.keys():
+            folder = os.path.join(grandparent_dir, categories[category])
+            category_funcs = find_pub_functions_in_folder(folder)
+            for func in category_funcs:
+                export_file.write(func)
+                export_file.write(';\n')
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <path_to_c_file>")
-        sys.exit(1)
-
-    file_path = sys.argv[1]
-    find_pub_functions(file_path)
+    generate_header()
