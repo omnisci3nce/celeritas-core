25aed9b0080ed99eac38006ef4e45480ff239d3a
omniscient <17525998+omnisci3nce@users.noreply.github.com>
2024-07-29 19:32:34 +1000

    hardcoded core_static

--- bindgen/rust/celeritas-sys/build.rs
+++ bindgen/rust/celeritas-sys/build.rs
@@ -33,8 +33,9 @@ fn main() {
     // TODO: we need to look based on OS
     // println!("cargo:rustc-link-search=../../build/windows/x64/debug");
     
-    let static_lib_path = std::env::var("CELERITAS_CORE_LIB")
-        .unwrap_or("../../../build/macosx/arm64/debug".to_string());
+    let static_lib_path = "/Users/josh/code/CodenameVentus/deps/celeritas-core/build/macosx/arm64/debug".to_string();
+    // let static_lib_path = std::env::var("CELERITAS_CORE_LIB")
+    //     .unwrap_or("../../../build/macosx/arm64/debug".to_string());
 
     println!("cargo:rustc-link-search={static_lib_path}");
 
